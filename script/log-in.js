3cfe2c9972eb8c8fcd132171b4b278a7a640ce01
fshippman <fshippman@yahoo.com>
2024-03-07 15:21:05 +0100

    login design

--- script/log-in.js
+++ script/log-in.js
@@ -29,18 +29,17 @@ function loadLogIn() {
  * Starts the join-logo animation if the document referrer is empty.
  */
 function startAnimation() {
-        document.querySelector('.join-logo-contain').classList.add('animated');
-        document.querySelector('.join-logo-contain').classList.remove('d-none');
-        document.querySelector('.join-logo').classList.add('animated');
+    document.querySelector('.join-logo-contain').classList.add('animated');
+    document.querySelector('.join-logo-contain').classList.remove('d-none');
+    document.querySelector('.join-logo').classList.add('animated');
 }
 
 /**
  * Redirects the user to the registration page.
  */
 function loadRegister() {
-    window.location.href = "./register.html";
+    window.location.href = './register.html';
 }
-
 
 /**
  * Validates user credentials and logs them in if valid.
@@ -49,21 +48,21 @@ function login() {
     let email = document.getElementById('login_email');
     let password = document.getElementById('login_password');
     if (verifyLoginCredentials(email, password)) {
-        handleRememberMe(email)
+        handleRememberMe(email);
         setActiveUser(email);
         saveActiveUser();
-        window.location.href = "./summary.html";
+        window.location.href = './summary.html';
     } else {
         loadRedBorderInput();
         loadWarningTextTamplate();
     }
 }
 
-function verifyLoginCredentials(email, password){
+function verifyLoginCredentials(email, password) {
     return allUsers.find(u => u.email === email.value && u.password === password.value);
 }
 
-function handleRememberMe(email){
+function handleRememberMe(email) {
     if (document.getElementById('myCheckbox').checked) {
         localStorage.setItem('rememberMe', email.value);
     } else {
@@ -71,11 +70,10 @@ function handleRememberMe(email){
     }
 }
 
-function setActiveUser(email){
+function setActiveUser(email) {
     let currentUser = allUsers.findIndex(u => u.email === email.value);
     activeUser['name'] = allUsers[currentUser].name;
 }
-
 
 /**
  * Logs in a user as a guest and fills default data arrays.
@@ -84,7 +82,7 @@ function guestLogin() {
     activeUser.name = 'Guest';
     saveActiveUser();
     fillTestArray();
-    window.location.href = "./summary.html";
+    window.location.href = './summary.html';
 }
 
 /**
@@ -93,130 +91,80 @@ function guestLogin() {
 function fillTestArray() {
     contactsArray = [
         {
-            "name": "Bernhard Sigl",
-            "nameAbbreviation": "BS",
-            "email": "B-Test@web.de",
-            "phone": "01631234567",
-            "color": "#006400"
+            'name': 'Bernhard Sigl',
+            'nameAbbreviation': 'BS',
+            'email': 'B-Test@web.de',
+            'phone': '01631234567',
+            'color': '#006400',
         },
         {
-            "name": "David Peterka",
-            "nameAbbreviation": "DP",
-            "email": "test@web.de",
-            "phone": "123456",
-            "color": "#00008B"
+            'name': 'David Peterka',
+            'nameAbbreviation': 'DP',
+            'email': 'test@web.de',
+            'phone': '123456',
+            'color': '#00008B',
         },
         {
-            "name": "Lina Wionsek",
-            "nameAbbreviation": "LW",
-            "email": "test2@web.de",
-            "phone": "123456",
-            "color": "#8B0000"
-        }];
+            'name': 'Lina Wionsek',
+            'nameAbbreviation': 'LW',
+            'email': 'test2@web.de',
+            'phone': '123456',
+            'color': '#8B0000',
+        },
+    ];
 
-    tasks = [{
-        "id": 3,
-        "status": "toDo",
-        "category": "Technical Task",
-        "categoryColor": "background: #1FD7C1",
-        "title": "first guest task",
-        "description": "text for task",
-        "dueDate": "22/10/2023",
-        "priority": "./img/prioUrgent.svg",
-        "contactName": [
-            "Bernhard Sigl",
-            "David Peterka",
-            "Lina Wionsek"
-        ],
-        "contactColor": [
-            "#006400",
-            "#00008B",
-            "#8B0000"
-        ],
-        "contactAbbreviation": [
-            "BS",
-            "DP",
-            "LW"
-        ],
-        "subtasksInProgress": [
-            "first subtask",
-            "second subtask",
-            "third subtask"
-        ],
-        "subtasksFinish": []
-    },
-    {
-        "id": 4,
-        "status": "toDo",
-        "category": "New Category",
-        "categoryColor": "background: #FF6347",
-        "title": "second guest task",
-        "description": "text for task",
-        "dueDate": "24/10/2023",
-        "priority": "./img/prioMedium.svg",
-        "contactName": [
-            "Bernhard Sigl",
-            "David Peterka",
-            "Lina Wionsek"
-        ],
-        "contactColor": [
-            "#006400",
-            "#00008B",
-            "#8B0000"
-        ],
-        "contactAbbreviation": [
-            "BS",
-            "DP",
-            "LW"
-        ],
-        "subtasksInProgress": [
-            "first subtask",
-            "second subtask",
-            "third subtask"
-        ],
-        "subtasksFinish": []
-    },
-    {
-        "id": 5,
-        "status": "awaiting-feedback",
-        "category": "User Story",
-        "categoryColor": "background: #0038FF",
-        "title": "third guest task",
-        "description": "text for task",
-        "dueDate": "21/10/2023",
-        "priority": "./img/prioUrgent.svg",
-        "contactName": [
-            "Bernhard Sigl",
-            "David Peterka",
-            "Lina Wionsek"
-        ],
-        "contactColor": [
-            "#006400",
-            "#00008B",
-            "#8B0000"
-        ],
-        "contactAbbreviation": [
-            "BS",
-            "DP",
-            "LW"
-        ],
-        "subtasksInProgress": [
-            "first subtask",
-            "second subtask",
-            "third subtask"
-        ],
-        "subtasksFinish": []
-    }
+    tasks = [
+        {
+            'id': 3,
+            'status': 'toDo',
+            'category': 'Technical Task',
+            'categoryColor': 'background: #1FD7C1',
+            'title': 'first guest task',
+            'description': 'text for task',
+            'dueDate': '22/10/2023',
+            'priority': './img/prioUrgent.svg',
+            'contactName': ['Bernhard Sigl', 'David Peterka', 'Lina Wionsek'],
+            'contactColor': ['#006400', '#00008B', '#8B0000'],
+            'contactAbbreviation': ['BS', 'DP', 'LW'],
+            'subtasksInProgress': ['first subtask', 'second subtask', 'third subtask'],
+            'subtasksFinish': [],
+        },
+        {
+            'id': 4,
+            'status': 'toDo',
+            'category': 'New Category',
+            'categoryColor': 'background: #FF6347',
+            'title': 'second guest task',
+            'description': 'text for task',
+            'dueDate': '24/10/2023',
+            'priority': './img/prioMedium.svg',
+            'contactName': ['Bernhard Sigl', 'David Peterka', 'Lina Wionsek'],
+            'contactColor': ['#006400', '#00008B', '#8B0000'],
+            'contactAbbreviation': ['BS', 'DP', 'LW'],
+            'subtasksInProgress': ['first subtask', 'second subtask', 'third subtask'],
+            'subtasksFinish': [],
+        },
+        {
+            'id': 5,
+            'status': 'awaiting-feedback',
+            'category': 'User Story',
+            'categoryColor': 'background: #0038FF',
+            'title': 'third guest task',
+            'description': 'text for task',
+            'dueDate': '21/10/2023',
+            'priority': './img/prioUrgent.svg',
+            'contactName': ['Bernhard Sigl', 'David Peterka', 'Lina Wionsek'],
+            'contactColor': ['#006400', '#00008B', '#8B0000'],
+            'contactAbbreviation': ['BS', 'DP', 'LW'],
+            'subtasksInProgress': ['first subtask', 'second subtask', 'third subtask'],
+            'subtasksFinish': [],
+        },
     ];
 
     allCategorys[0] = {
-        "name": [
-            "New Category"
-        ],
-        "color": [
-            "background: #FF6347"
-        ]
-    }
+        'name': ['New Category'],
+        'color': ['background: #FF6347'],
+    };
     currentUserTaskSave();
     currentUserCategorysSave();
     currentUserContactsSave();
@@ -226,9 +174,9 @@ function fillTestArray() {
  * Adds a red border to specified input elements indicating an error.
  */
 function loadRedBorderInput() {
-    let inputIds = ["login_email", "login_password"];
+    let inputIds = ['login_email', 'login_password'];
     for (let id of inputIds) {
-        document.getElementById(id).classList.add("red-border");
+        document.getElementById(id).classList.add('red-border');
     }
 }
 
@@ -236,9 +184,9 @@ function loadRedBorderInput() {
  * Displays warning text templates for specified elements.
  */
 function loadWarningTextTamplate() {
-    let warningIds = ["warning-text-passwort", "warning-text-email"];
+    let warningIds = ['warning-text-passwort', 'warning-text-email'];
     for (let id of warningIds) {
-        document.getElementById(id).classList.remove("d-none");
+        document.getElementById(id).classList.remove('d-none');
     }
 }
 
@@ -248,41 +196,36 @@ function loadWarningTextTamplate() {
 function loadTempleteLogIn() {
     return /*html*/ `
     <form onsubmit="login(); return false;">
-        <div class="logInTitle">
-            <div class="fontSize61"><b>Log in</b></div>
-            <div class="underline"></div>
-        </div>
-        <div  class="input-fields fontSize20">
-            <!-- <div id="login_email_container" class="input-field "> -->
+            <div class="logInTitle">
+                <div class="fontSize61"><b>Log in</b></div>
+                <div class="underline"></div>
+            </div>
+            <div class="input-fields fontSize20">
                 <input required id="login_email" type="email" placeholder="Email">
-                <!-- <img src="./img/letter.svg" alt="Bild hinten" class="input-suffix">
-            </div> -->
-            <div class="warning-field">
-            <span id="warning-text-email" class="d-none">
-            Please enter the appropriate email address.
-            </span>
-            </div>
-            <!-- <div id="login_password_container" class="input-field"> -->
-                <input required id="login_password" type="password" placeholder="Password">
-                <!-- <img src="./img/lock.svg" alt="Bild hinten" class="input-suffix"> -->
-            <!-- </div> -->
-            <div class="warning-field">
-            <span id="warning-text-passwort" class="d-none">
-            Please enter the appropriate password.
-            </span>
-            </div>
-        </div>
-
-        <div class="remember-me-container">
-            <div class="checkboxRememberMeContainer">
-                <input type="checkbox" name="myCheckbox" id="myCheckbox">
-                <label for=" fontSize16">Remember me</label>
+                <div class="warning-field">
+                    <span id="warning-text-email" class="d-none">
+                        Please enter the appropriate email address. 
+                    </span>
+                </div>
+                <input class="mt-32" required id="login_password" type="password" placeholder="Password">  
+                <div class="warning-field">
+                    <span id="warning-text-passwort" class="d-none">
+                        Please enter the appropriate password.
+                    </span>
+                </div>
             </div>
 
-        </div>
-        <div class="buttonsUnderLogin">
-            <button type="submit" class="button-log-in fontSize21">Log in</button>
-            <button onclick='guestLogin()' type="button" class="button-guest-login fontSize21">Guest Log in</button>
-        </div>
+            <div class="remember-me-container">
+                <div class="checkboxRememberMeContainer">
+                    <input type="checkbox" name="myCheckbox" id="myCheckbox">
+                    <label for=" fontSize16">Remember me</label>
+                </div>
+
+            </div>
+            <div class="buttonsUnderLogin">
+                <button type="submit" class="btn blue-btn border-radius-8 fontSize21">Log in</button>
+                <button onclick='guestLogin()' type="button" class="btn outline-btn border-radius-8 fontSize21">Guest Log in</button>
+            </div>
+
     </form>`;
 }
