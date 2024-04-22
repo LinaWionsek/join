6d4afe0f5439f0416aa648deec6e3b469233f92c
fshippman <fshippman@yahoo.com>
2024-04-22 16:13:33 +0200

    summary sirt

--- script/summary.js
+++ script/summary.js
@@ -164,7 +164,6 @@ function getNextUrgentDueDate(tasks) {
  */
 function convertDateFormat(inputDate) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
     const parts = inputDate.split("-");
     const year = parts[0];
     const monthIndex = parseInt(parts[1], 10) - 1;
